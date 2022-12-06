699feb5c0fd1097e5ad00373fc0528b18c8b0cb7
Tomas Centurion <TomasCentu98@yahoo.com>
2022-12-06 04:00:55 -0300

    agregado validaciones de formulario y modo oscuro

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,2 +1,44 @@
+// para dark-mode 
+
+const check = document.getElementById('mode');
+
+check.addEventListener('change', () => {
+    document.body.classList.toggle('black');
+});
+
 // para validacion de formularios
-console.log('hola');
+
+const mensajeDeError = {
+    nombre: {
+        valueMissing: "este campo no puede estar vacio"
+    },
+    email: {
+        valueMissing: "este campo no puede estar vacio",
+        typeMismatch: "el correo no es valido"
+    },
+    asunto: {
+        valueMissing: "este campo no puede estar vacio",
+        patterMismatch: "la contraseña no es valida"
+    }
+}
+
+const inputs = document.querySelectorAll("input");
+
+function validar(input) {
+
+    let span = input.parentElement.querySelector(".input-message-error");
+
+    if (input.checkValidity()) {
+        input.classList.remove("error");  
+        span.style.visibility = "hidden";     
+    } else {
+        input.classList.add("error");  
+        span.style.visibility = "visible";    
+    }
+}
+
+inputs.forEach( input => {
+    input.addEventListener("blur", (input) => {
+        validar(input.target);
+    })
+})
