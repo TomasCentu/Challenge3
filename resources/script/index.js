6be7353b1d22bca099e9eb24be6872bbc64fb3f6
Tomas Centurion <TomasCentu98@yahoo.com>
2022-12-06 19:03:05 -0300

    detalles con JS para estilos y scrolls

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,29 +1,19 @@
-// para dark-mode 
-
+// *** para dark-mode ***
 const check = document.getElementById('mode');
 
+// solo agrega el estilo a la etiqueta body para
+// que se apliquen los estilos del 'modo-oscuro'
 check.addEventListener('change', () => {
     document.body.classList.toggle('black');
 });
 
-// para validacion de formularios
-
-const mensajeDeError = {
-    nombre: {
-        valueMissing: "este campo no puede estar vacio"
-    },
-    email: {
-        valueMissing: "este campo no puede estar vacio",
-        typeMismatch: "el correo no es valido"
-    },
-    asunto: {
-        valueMissing: "este campo no puede estar vacio",
-        patterMismatch: "la contraseña no es valida"
-    }
-}
+// *** para validacion de formularios ***
 
 const inputs = document.querySelectorAll("input");
 
+// funcion para validar el contenido de los inputs
+// si el contenido no corresponde con su formato cambia el color a error
+// y hace visible el mensaje de error de la etiqueta span
 function validar(input) {
 
     let span = input.parentElement.querySelector(".input-message-error");
@@ -37,8 +27,52 @@ function validar(input) {
     }
 }
 
+// revisa cada input cuando se selecciona y se sale
+// y llama la funcion para verificar que su contenido sea correcto o no
+// poniendo los estilos de error cuando sea incorrecto el contenido
 inputs.forEach( input => {
     input.addEventListener("blur", (input) => {
         validar(input.target);
     })
 })
+
+// *** para el scroll de la pagina ***
+
+// al hacer click en mi nombre del nav
+// va hacia arriba del todo de la pagina, al inicio
+const nombre = document.querySelector('.name');
+
+nombre.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
+const nav = document.querySelectorAll('a.nav_item_a');
+
+// se fija que ningun link del nav tenga el borde activado
+// y solo activa al que clickeemos 
+const marcar = (link) => {
+    nav.forEach((item) => {
+        item.style.borderBottom = 'none';
+    });
+
+    link.style.borderBottom = '3px solid #037f8c';
+};
+
+// agarra el tamaño del nav cada vez que se hace click en un link del mismo
+// y ajusta el scroll para que vaya de forma correcta a la seccion
+nav.forEach(link => {
+    link.addEventListener('click', (link) => {
+        let navHeigth = document.querySelector('.header_conteiner').clientHeight;
+        const tagStyle = document.querySelector("style.nav");
+        let textStyle = `*{scroll-margin-block-start: ${navHeigth + 'px'}; scroll-behavior: smooth;}`;
+        
+        if (tagStyle.innerHTML = '') {
+            tagStyle.appendChild(textStyle);
+        } else {
+            tagStyle.innerHTML = textStyle;
+        }
+
+        marcar(link.target);
+    });
+});
+
+
